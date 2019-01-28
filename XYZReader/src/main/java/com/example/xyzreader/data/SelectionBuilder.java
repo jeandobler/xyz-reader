df3eb6cac8124104d63b28e0b87862b3eb1fea77
Jean Dobler <jedobler@gmail.com>
2019-01-27 22:24:50 -0200

    Corrigindo problemas da revisão

--- XYZReader/src/main/java/com/example/xyzreader/data/SelectionBuilder.java
+++ XYZReader/src/main/java/com/example/xyzreader/data/SelectionBuilder.java
@@ -42,22 +42,6 @@ public class SelectionBuilder {
     private StringBuilder mSelection;
     private ArrayList<String> mSelectionArgs;
 
-    /**
-     * Reset any internal state, allowing this builder to be recycled.
-     */
-    public SelectionBuilder reset() {
-        mTable = null;
-		if (mProjectionMap != null) {
-			mProjectionMap.clear();
-		}
-		if (mSelection != null) {
-			mSelection.setLength(0);
-		}
-		if (mSelectionArgs != null) {
-			mSelectionArgs.clear();
-		}
-        return this;
-    }
 
     /**
      * Append the given selection clause to the internal state. Each clause is
@@ -117,18 +101,6 @@ public class SelectionBuilder {
     	if (mSelectionArgs == null) {
     		mSelectionArgs = new ArrayList<String>();
     	}
-    }
-
-    public SelectionBuilder mapToTable(String column, String table) {
-    	ensureProjectionMap();
-        mProjectionMap.put(column, table + "." + column);
-        return this;
-    }
-
-    public SelectionBuilder map(String fromColumn, String toClause) {
-    	ensureProjectionMap();
-        mProjectionMap.put(fromColumn, toClause + " AS " + fromColumn);
-        return this;
     }
 
     /**
